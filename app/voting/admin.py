0924a1e8151eb336b324bc999ab205e6f7bafa3d
NickPYTH <nicksrnk@gmail.com>
2020-12-02 19:45:09 +0300

    Added filters to admin page

--- app/voting/admin.py
+++ app/voting/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
 from .models import Список_вопросов, Список_ответов, Список_ПВИ, Дата_окончания_голосования, Общие_комментарии
-# Register your models here.
 
 
-admin.site.register(Список_вопросов)
-admin.site.register(Список_ответов)
-admin.site.register(Список_ПВИ)
-admin.site.register(Дата_окончания_голосования)
-admin.site.register(Общие_комментарии)
+@admin.register(Список_вопросов)
+class Список_вопросовAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "date")
 
-#@admin.register(Список_вопросов)
-#class Список_вопросовAdmin(admin.ModelAdmin):
-#    list_display = ("unique_key", "Вопрос")
+@admin.register(Список_ответов)
+class Список_ответовAdmin(admin.ModelAdmin):
+    list_display = ("unique_key", "Вопрос" ,"ПВИ", "Оценка", )
+    list_filter = ("ПВИ", "Вопрос" , "Оценка", )
 
+@admin.register(Список_ПВИ)
+class Список_ПВИAdmin(admin.ModelAdmin):
+    list_display = ("Название_ПВИ", "Местонахождение_ПВИ")
 
+@admin.register(Дата_окончания_голосования)
+class Дата_окончания_голосованияAdmin(admin.ModelAdmin):
+    pass
 
+@admin.register(Общие_комментарии)
+class Общие_комментарииAdmin(admin.ModelAdmin):
+    pass
